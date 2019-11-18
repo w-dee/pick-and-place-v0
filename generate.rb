4a3230073a48d843fef68e2ce4207c76fa4dbfe8
W.Dee <dee@kikyou.info>
2019-11-18 13:40:26 +0900

    Added rotation / tilt compensation

--- generate.rb
+++ generate.rb
@@ -9,16 +9,15 @@ z_lift = z_feeder + 10.0 # lift z position
 
 z_slow_margin = 1 # slowdown margin around pick/place
 
-x_count = 48
-y_count = 54
+x_count = 10
+y_count = 10
 
-x_pitch = 3.85
-y_pitch = 3.83075
+cp=[]
 
-x_start = -15.00 # x_start position
-y_start = -11.00 # y_start position
-x_end = x_start + (x_count-1) * x_pitch # x_end position
-y_end = y_start + (y_count-1) * y_pitch # y_end position
+cp[0] = [90.0, 0.0] # lower-left corner position
+cp[1] = [90.0, 90.0] # lower-right corner position
+cp[2] = [0.0,  0.0] # upper-left corner position
+cp[3] = [0.0, 90.0] # upper-right corner position
 
 
 confirm_count = 8
@@ -42,7 +41,7 @@ gcode_pump_off = "M400\nM107\nG4 S1"
 gcode_feed = "M400\nM42 P211 S255\nG4 P50\nM42 P211 S0"
 
 
-bezier_intp_points = 100
+bezier_intp_points = 3
 
 gcode_start ="
 M80 ; power on
@@ -61,35 +60,6 @@ G1 Z#{z_lift.r}
 "
 
 print gcode_start
-
-if ARGV[0] == "-b" then
-
-	# repeat visiting four corners of the target area
-	print "G1 F#{unloaded_move_speed.r}\n"
-	10 .times do
-		print "G1 Z#{(z_target + z_slow_margin).r}\n"
-		print "G1 X#{x_start.r} Y#{y_start.r}\n"
-		print "G1 Z#{z_target.r}\n"
-		print "M0\n"
-		print "G1 Z#{(z_target + z_slow_margin).r}\n"
-		print "G1 X#{x_end.r} Y#{y_start.r}\n"
-		print "G1 Z#{z_target.r}\n"
-		print "M0\n"
-		print "G1 Z#{(z_target + z_slow_margin).r}\n"
-		print "G1 X#{x_end.r} Y#{y_end.r}\n"
-		print "G1 Z#{z_target.r}\n"
-		print "M0\n"
-		print "G1 Z#{(z_target + z_slow_margin).r}\n"
-		print "G1 X#{x_start.r} Y#{y_end.r}\n"
-		print "G1 Z#{z_target.r}\n"
-		print "M0\n"
-		print "\n"
-	end
-	exit 0
-end
-
-
-
 
 
 # initialize the list
@@ -112,6 +82,55 @@ if 1 then
 
 end
 
+
+
+
+
+# projective transformation
+require 'matrix'
+
+
+def conv1(x1, y1, x2, y2, x3, y3, x4, y4) 
+	m = Matrix[
+    [x1, x2, x3 ],
+    [y1, y2, y3 ],
+    [ 1.0,  1.0,  1.0 ]
+        ]
+    v = m.adjugate * Vector[x4, y4, 1.0]
+    return m * Matrix[
+    [v[0], 0.0, 0.0 ],
+    [0.0, v[1], 0.0 ],
+    [0.0, 0.0, v[2] ]
+         ]
+end
+
+def calc_mat(
+  x1s, y1s, x1d, y1d,
+  x2s, y2s, x2d, y2d,
+  x3s, y3s, x3d, y3d,
+  x4s, y4s, x4d, y4d)
+
+    s = conv1(x1s, y1s, x2s, y2s, x3s, y3s, x4s, y4s)
+    d = conv1(x1d, y1d, x2d, y2d, x3d, y3d, x4d, y4d)
+    return d * s.adjugate
+end
+
+
+def trans(mat, x, y)
+	v = mat * Vector[x, y, 1.0]
+	return [v[0] / v[2], 
+			v[1] / v[2] ]
+end
+
+matrix = calc_mat(
+	0.0,         0.0,            cp[0][0].to_f, cp[0][1].to_f,
+	x_count-1.0, 0.0,            cp[1][0].to_f, cp[1][1].to_f,
+	0.0, y_count-1.0,            cp[2][0].to_f, cp[2][1].to_f,
+	x_count-1.0, y_count-1.0,    cp[3][0].to_f, cp[3][1].to_f)
+
+
+
+# bezier interpolation
 def intp3d(a, b, t)
 	rt = 1 - t
 	[rt * a[0] + t * b[0],  rt * a[1] + t * b[1],  rt * a[2] + t * b[2] ]
@@ -138,14 +157,13 @@ end
 
 # iterate list
 cnt = 0
-y_last = y_start
-x_last = x_start
+y_last = cp[0][1]
+x_last = cp[0][0]
 z_last = z_lift
 for v in list do
 	x, y = v
 
-	target_x = x_pitch * x + x_start
-	target_y = y_pitch * y + y_start
+	target_x, target_y = trans(matrix, x.to_f, y.to_f)
 
 	print "M117 x #{x}, y #{y} @ #{target_x.r}, #{target_y.r}\n"
 
